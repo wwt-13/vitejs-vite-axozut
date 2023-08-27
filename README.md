449afb7c629224abf8fd98da7d1b6a87c20f85c2
wwt-13 <79509720+wwt-13@users.noreply.github.com>
2023-08-27 10:29:12 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# chatkitty-vue-example
+
+[vue-advanced-chat](https://github.com/antoine92190/vue-advanced-chat) is a feature-rich and highly customizable
+Vue chat component library designed to simplify the process of building modern, real-time chat applications.
+It provides a wide range of out-of-the-box features, including:
+
+- Direct messaging and group chats
+- Images, videos, files, voice messages, emojis and link previews
+- Tag users & emojis shortcut suggestions
+- Typing indicators
+- Reactions with emojis and GIFs
+- Markdown text formatting - bold, italic, strikethrough, underline, code, multiline, etc.
+- Online presence indicators for online/offline users' status
+- Delivery and read receipts
+- Theming and customization options including light and dark theme modes
+- Responsive design perfect for mobile
+
+In addition, vue-advanced-chat is compatible with all Javascript frameworks (Vue, Angular, React, etc.)
+or no framework at all as a [web component](https://kinsta.com/blog/web-components/).
+
+By using vue-advanced-chat alongside a chat API service like ChatKitty, developers can quickly create
+chat applications with minimal boilerplate code, while also benefiting from a user-friendly and customizable UI.
+
+This library helps reduce the time and effort required to implement chat functionalities, allowing developers
+to focus on other aspects of their application. Moreover, its responsive design ensures the chat interface
+adapts to different screen sizes, making it suitable for web and mobile applications alike.
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+### Lint with [ESLint](https://eslint.org/)
+
+```sh
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,143 @@
+<script setup lang="ts">
+import ChatComponent from './components/ChatComponent.vue'
+import { ref } from 'vue'
+
+const theme = ref('light')
+
+const users = [
+  {
+    username: 'b2a6da08-88bf-4778-b993-7234e6d8a3ff',
+    name: 'Joni'
+  },
+  {
+    username: 'abc4264d-f1b1-41c0-b4cc-1e9daadfc893',
+    name: 'Penelope'
+  },
+  {
+    username: 'c6f75947-af48-4893-a78e-0e0b9bd68580',
+    name: 'Julie'
+  },
+  {
+    username: '2989c53a-d0c5-4222-af8d-fbf7b0c74ec6',
+    name: 'Paxton'
+  },
+  {
+    username: '8fadc920-f3e6-49ff-9398-1e58b3dc44dd',
+    name: 'Zaria'
+  }
+]
+
+const username = ref(users[Math.floor(Math.random() * users.length)].username)
+</script>
+
+<template>
+  <div class="app-container" :class="{ 'app-container-dark': theme === 'dark' }">
+    <span class="user-logged" :class="{ 'user-logged-dark': theme === 'dark' }">
+      Logged in as
+    </span>
+    <select v-model="username">
+      <option v-for="user in users" :key="user.username" :value="user.username">
+        {{ user.name }}
+      </option>
+    </select>
+
+    <div class="button-theme">
+      <button class="button-light" @click="theme = 'light'">Light</button>
+      <button class="button-dark" @click="theme = 'dark'">Dark</button>
+    </div>
+
+    <ChatComponent :theme="theme" :username="username" />
+  </div>
+</template>
+
+<style lang="scss">
+body {
+  margin: 0;
+}
+
+input {
+  -webkit-appearance: none;
+}
+
+.app-container {
+  font-family: 'Quicksand', sans-serif;
+  padding: 30px;
+
+  &.app-container-dark {
+    background: #131415;
+  }
+}
+
+.user-logged {
+  font-size: 12px;
+  margin-right: 5px;
+  margin-top: 10px;
+
+  &.user-logged-dark {
+    color: #fff;
+  }
+}
+
+select {
+  height: 20px;
+  outline: none;
+  border: 1px solid #e0e2e4;
+  border-radius: 4px;
+  background: #fff;
+  margin-bottom: 20px;
+}
+
+.button-theme {
+  float: right;
+  display: flex;
+  align-items: center;
+
+  .button-light {
+    background: #fff;
+    border: 1px solid #46484e;
+    color: #46484e;
+  }
+
+  .button-dark {
+    background: #1c1d21;
+    border: 1px solid #1c1d21;
+  }
+
+  button {
+    color: #fff;
+    outline: none;
+    cursor: pointer;
+    border-radius: 4px;
+    padding: 6px 12px;
+    margin-left: 10px;
+    border: none;
+    font-size: 14px;
+    transition: 0.3s;
+    vertical-align: middle;
+
+    &.button-github {
+      height: 30px;
+      background: none;
+      padding: 0;
+      margin-left: 20px;
+
+      img {
+        height: 30px;
+      }
+    }
+
+    &:hover {
+      opacity: 0.8;
+    }
+
+    &:active {
+      opacity: 0.6;
+    }
+
+    @media only screen and (max-width: 768px) {
+      padding: 3px 6px;
+      font-size: 13px;
+    }
+  }
+}
+</style>
